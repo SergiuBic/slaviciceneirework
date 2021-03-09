88f195781b85cbd9add63f36543dad5124d2bb90
SergiuBic <wikin_n@yahoo.com>
2021-03-09 10:55:25 +0200

    yolo'

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -3,19 +3,19 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages2"]);
+    await execa("git", ["checkout", "--orphan", "gh-pages5"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages5"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
+    await execa("git", ["branch", "-D", "gh-pages5"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
     // eslint-disable-next-line no-console
